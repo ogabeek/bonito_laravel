48fb3db5879c8eb8efd0957b7f377a946ff2f524
ogabek <ogabek.masharipov@harbour.space>
2025-11-12 15:39:58 +0100

    feat: new routes for /hello, /greeting, /profile/ip

---
+++ resources/views/greeting.blade.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Greeting Page</title>
+</head>
+<body>
+    <h1>Welcome to Laravel!</h1>
+    <p>This is your first view {{$name}}</p>
+    <p> YOUR IP is {{$ip}} <p>
+</body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -6,4 +6,23 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+Route::get('/hello', function(){
+    return 'Hello world! 🪴';
+});
+
+Route::get('/greet/{name}', function($name){
+    return "Hello, $name! 🪴";
+});
+
+
+Route::get('/greeting', function(){
+    return view('greeting');
+});
+
+Route::get('/profile/{name}/{ip}', function($name, $ip){
+    return view('greeting', ['name'=> $name, 'ip' => $ip]);
+});
+
+
+
 // dd("hello");
