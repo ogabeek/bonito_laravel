655f1387b42e12b01cc8b0436c5eccf3d0abc338
ogabek <ogabek.masharipov@harbour.space>
2025-11-15 23:17:04 +0100

    renamed class to lesson and migrated

--- database/migrations/2025_11_15_221200_rename_classes_table_to_lessons.php
+++ database/migrations/2025_11_15_221200_rename_classes_table_to_lessons.php
@@ -11,9 +11,7 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::table('lessons', function (Blueprint $table) {
-            //
-        });
+        Schema::rename('classes','lessons');
     }
 
     /**
@@ -21,8 +19,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::table('lessons', function (Blueprint $table) {
-            //
-        });
+        Schema::rename('lessons', 'classes');
     }
 };
