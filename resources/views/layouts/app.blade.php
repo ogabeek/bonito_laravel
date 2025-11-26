dcb711795161425f1a7bc3813aa344f7e7af54fa
ogabek <ogabek.masharipov@harbour.space>
2025-11-26 21:07:20 +0100

    same as previous + N+1 problem fixed 4 steps ago actually!

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -6,7 +6,7 @@
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <title>@yield('title', 'Online School Management')</title>
     
-    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
     <script src="https://cdn.tailwindcss.com"></script>
     <script defer src="https://cdn.jsdelivr.net/npm/alpinejs@3.x.x/dist/cdn.min.js"></script>
     
